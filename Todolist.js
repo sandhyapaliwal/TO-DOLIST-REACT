edeb86e4bd3044fe238a23cebe3c9a276abf6581
sandhya paliwal <100416191+sandhyapaliwal@users.noreply.github.com>
2026-03-04 23:09:48 +0530

    Implement edit feature for Todo list items
    Added editing functionality to the Todo list item, allowing users to edit the text and save changes.

--- Todolist.js
+++ Todolist.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todolist(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(props.item);
+
+  const handleEdit = () => {
+    if (isEditing && editText.trim() !== '') {
+      props.editItem(props.index, editText);
+    }
+    setIsEditing(!isEditing);
+  }
+
   return (
     <li className="list-item">
-        {props.item}
-        <span className='icons'>
-        <i className="fa-solid fa-trash-can icon-delete" 
-        onClick={e=>{
-            props.deleteItem(props.index)
-        }}></i>
-        </span>
+      {isEditing ? (
+        <input
+          className="edit-input"
+          value={editText}
+          onChange={(e) => setEditText(e.target.value)}
+        />
+      ) : (
+        props.item
+      )}
+      <span className='icons'>
+        <i
+          className={`fa-solid ${isEditing ? 'fa-check' : 'fa-pen'} icon-edit`}
+          onClick={handleEdit}
+        ></i>
+        <i
+          className="fa-solid fa-trash-can icon-delete"
+          onClick={() => props.deleteItem(props.index)}
+        ></i>
+      </span>
     </li>
   )
 }

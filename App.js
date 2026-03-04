2688c3b0c65dc4fae6eccff28d45bd4f44a4b209
sandhya paliwal <100416191+sandhyapaliwal@users.noreply.github.com>
2026-03-04 23:08:55 +0530

    Refactor App component to use localStorage for todos

--- App.js
+++ App.js
@@ -1,27 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./App.css"
 import TodoInput from './components/Todoinput'
 import Todolist from './components/Todolist';
+
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
+  const [listTodo, setListTodo] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(listTodo));
+  }, [listTodo]);
+
+  let addList = (inputText) => {
+    if (inputText !== '')
+      setListTodo([...listTodo, { text: inputText, isEditing: false }]);
   }
-  const deleteListItem = (key)=>{
+
+  const deleteListItem = (key) => {
     let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
+    newListTodo.splice(key, 1);
+    setListTodo([...newListTodo]);
   }
+
+  const editListItem = (key, newText) => {
+    let newListTodo = [...listTodo];
+    newListTodo[key] = { text: newText, isEditing: false };
+    setListTodo([...newListTodo]);
+  }
+
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput addList={addList}/>
+        <TodoInput addList={addList} />
         <h1 className="app-heading">TO-DO LIST</h1>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
+        <hr />
+        {listTodo.map((listItem, i) => {
           return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+            <Todolist
+              key={i}
+              index={i}
+              item={listItem.text}
+              deleteItem={deleteListItem}
+              editItem={editListItem}
+            />
           )
         })}
       </div>
